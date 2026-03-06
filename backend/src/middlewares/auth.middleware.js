04d019dd1efcdc92c762c962587d2708e0429f11
Arihant Kumar <kumararihant.iiitm@gmail.com>
2026-03-06 22:22:25 +0530

    Bug fixed in auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,30 +1,33 @@
 import jwt from 'jsonwebtoken';
 import redisClient from '../services/redis.service.js';
+
 export const authMiddleware = async (req, res, next) => {
     try {
-        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
-        if(!token){
+        const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
             return res.status(401).json({
-                success:false,
+                success: false,
                 message: "Unauthorized User."
-            })
+            });
         }
 
         const isBlacklisted = await redisClient.get(token);
-        if(isBlacklisted){
+        if (isBlacklisted) {
             res.cookie('token', '');
             return res.status(400).json({
                 success: false,
                 message: "Unauthorized User."
-            })
+            });
         }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded
+        req.user = decoded;
         next();
-    } catch (error) {       
+    } catch (error) {
         return res.status(401).json({
             success: false,
-            message:'Unauthorized User.'
-        })
+            message: 'Unauthorized User.'
+        });
     }
 }
